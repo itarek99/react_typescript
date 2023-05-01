66413a3701f94e6c11bf9af8fc9d6ebc0bff02b1
itarek99 <tarekulislam7060@gmail.com>
2023-05-01 13:10:15 +0600

    polymorphic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import UserContextProvider from "./components/context/UserContext";
+import Text from "./components/polymorphic/Text";
 
 const App = () => {
   return (
     <UserContextProvider>
       <div>Hello</div>
+      <Text as="h1" children="Hello World" />
     </UserContextProvider>
   );
 };
