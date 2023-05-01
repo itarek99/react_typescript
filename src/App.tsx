0e000187908864bad0a5f75d9d63b188b2e973c9
itarek99 <tarekulislam7060@gmail.com>
2023-05-01 11:18:14 +0600

    template literals

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import UserContextProvider from "./components/context/UserContext";
-import RandomNumber from "./components/restriction/RandomNumber";
+import Toast from "./components/templateLiterals/Toast";
 
 const App = () => {
   return (
     <UserContextProvider>
-      <div className="container mx-auto px-4 flex justify-center items-center h-screen gap-6">
-        <RandomNumber value={150} negative />
-      </div>
+      <Toast position="center" />
     </UserContextProvider>
   );
 };
